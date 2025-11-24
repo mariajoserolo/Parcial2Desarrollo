1666c47a0bf00bdda0e7eb1f3c4442d5c2666066
Maria Jose Rodriguez <mj.rodriguezl1@uniandes.edu.co>
2025-11-24 14:34:45 -0500

    Commit inicial :))

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
